67b091aa6ac5772f8b01e05bc0f77bc8615205f0
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-08-30 14:29:36 -0300

    ultilizando_o_modo_'strict'_e fazendo_a_declaracao_da_variavel_isto_faz_com_que_a_programacao_seja_mais_limpa_e_funcional

---
+++ estrito.js
@@ -0,0 +1,6 @@
+"use strict"
+
+let nome = "Luis henrique silva ferreira"           /* com a atribuição do modo 'estrito' é necessario declarar a variável */
+console.log("Profissional de TI")           /* 'console.log' pode imprimir 'string' */
+console.log(nome)                       /* variavel */
+console.log("nome: " + nome)       /* e fazer concatenação */
