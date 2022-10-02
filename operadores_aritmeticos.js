0028c791503f048bfb4102e86ebd715299c6926b
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-02 01:47:42 -0300

    ultilizando_operadores_aritmeticos_de_'soma'_'subtracao'_e_'multiplicacao'

---
+++ operadores_aritmeticos.js
@@ -0,0 +1,12 @@
+let num1 = 0, num2 = 0, res1 = 0, res2 = 0
+
+num1 = 5        /* atribuindo valores as variaveis 'num1' e 'num2' */
+num2 = 10
+
+res1 = num1 + num2       /* fazendo a soma com o operador aritmetico '+' */
+res2 = (num1 + num2) * 2
+
+
+console.log(res1)        /* fazendo a impressao da soma */
+console.log(res2)           /* por usar () a multiplicação sera feita depois da soma */    
+console.log(num2 - num1)        /* fazendo calculo dentro do 'console' */
