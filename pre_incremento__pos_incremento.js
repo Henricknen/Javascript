e138bde2a52e9e1ba93197e7226b8dacf1bf2baf
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-03 16:19:42 -0300

    operacoes_de_pos_pre_incremento_e_sempre_ultilizado_primeiro_o_elemento_da_esquerda

---
+++ pre_incremento__pos_incremento.js
@@ -0,0 +1,18 @@
+let n1 = 10
+let n2 = 10
+let n3 = -10
+let x = -n3     /* o operador '--' é um operador de inversão */
+
+n1++     /* '++' na direita do valor estarei usuando-o e depois incrementando o valor isto é chamado de pós incremento */
+
+console.log(n1)
+
+console.log(++n2)       /* '++' a esquerda do valor etou incrementado-o primeiro e depois usando */
+
+console.log(n2)     /* mesmo mesmo 'n2' instanciado com o valor 10 ele sera apresentado com 11 pois ja foi pré incrementado */
+
+console.log(--n1)       /* ultilizando '--' estou fazendo um decremento */
+
+console.log(x)      /* 'x' apresentará 10 positivo pois o '-' inverteu 'n3' */
+
+
