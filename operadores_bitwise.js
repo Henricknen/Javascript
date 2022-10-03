d37e3cf5498eec5580a411bf5d71fd4b9e865b6b
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-02 23:19:14 -0300

    operadores_'bitwise'_sao_operadores_que_operam_'bit'_a_'bit'_em_javascript

---
+++ operadores_bitwise.js
@@ -0,0 +1,18 @@
+let n1 = 10
+let n2 = 14
+
+let res = n1 & n2       /* ultilizando '&&' faz a operação 'E' no Bit a Bit */
+
+let ress = n1 | n2      /* '|' faz a operação 'OU' */
+
+let resss = n1 ^ n2
+
+let ressss = n1 << 1    /* deslocando um Bit para esquerda estarei dobrando o  valor do Bit */
+
+let resssss = n1 >> 1   /* deslocando o Bit para a direita é o mesmo que reduzi-lo pela metade */
+
+console.log(res)
+console.log(ress)
+console.log(resss)
+console.log(ressss)
+console.log(resssss)
