60634153217babd66771b3512067a1a30cf142ed
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-02 21:43:02 -0300

    ultilizando_operadores_'relacionais'_que_fazem_comparacoes_entre_variaveis

---
+++ operadores_relacionais.js
@@ -0,0 +1,15 @@
+let num1 = 10
+let num2 = 5
+let num3 = 10
+
+console.log(num1 > num2)    /* este 'console' retornara 'true' */
+
+console.log(num1 < num2)        /* ja este retornara 'false' */    
+
+console.log(num1 >= num3)           /* este retornara 'true' pois 'num1' não é maior mas é igual a 'num3' */
+
+console.log(num1 == num3)           /* retornara 'true' se as variaveis forem iguais */
+
+console.log(!(num1 == num3))    /* '!' este operador faz uma negação invertendo o resultado */
+
+console.log(num1 != num3)   /* '!=' operador cjama 'diferente' */
