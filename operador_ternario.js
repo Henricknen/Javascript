9b9eae6338222df00b0f3302c6ac9189cf5ab22e
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-03 16:55:41 -0300

    ultilizando_operador_ternario_'?'_sintaxe_da_operacao_ternaria_'teste_logico_?_se_veraddeiro_:_se_falso'

---
+++ operador_ternario.js
@@ -0,0 +1,5 @@
+let num = 10    /* 0 = False / 1 = True */
+         /* verdadeiro / falso */
+res = (!(num % 2) ? "Par" : "Impar")      /* '?' é o operador ternario '!' nega 'num % 2' que dara 0 e iria retorna 'False' */
+
+console.log(res)
