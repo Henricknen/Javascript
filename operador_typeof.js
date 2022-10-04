4d516edc3c1a9b472651f0a6d3fa482417af6748
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-04 10:33:21 -0300

    'typeof'_retorna_o_tipo_de_dados_da_variaveis

---
+++ operador_typeof.js
@@ -0,0 +1,10 @@
+let v1 = 10
+let v2 = "10"
+let v3 = v1 === v2
+let v4 = {nome: "Luis henrique s f"}
+
+console.log("Valor: " + v1 + " - Tipo: " + typeof(v1))
+console.log("Valor: " + v2 + " - Tipo: " + typeof(v2))
+console.log("Valor: " + v3 + " - Tipo: " + typeof(v3))
+console.log("Valor: " + v4 + " - Tipo: " + typeof(v4))
+
