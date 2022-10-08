b79474fd803252b3ce68faca94d1bac477fdf052
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-07 22:39:46 -0300

    operador_'spread'_quebra_ou_seja_espalha_um_array_e_devolve_elemento_por_elemento_deste array

---
+++ operador_spread.js
@@ -0,0 +1,7 @@
+let n1 = [10, 20, 30]       /* array 'n1' */
+let n2 = [40, 50, 60, 70, 80]       /* array 'n2' */
+let n3 = [...n1,...n2]       /* 'n3' ultilizando o operador 'spread' */
+
+
+console.log("n3 :" + n3)
+console.log("tipo de n3: " + typeof(n3))
