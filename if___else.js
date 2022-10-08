fa5cc40a8bd3c241d9f3edbd9eca3dee435ab9b7
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-08 20:08:55 -0300

    fazendo_'alinhamento'_de_'if'_inserindo_um_'if'_dentro_de_outro

---
+++ if___else.js
@@ -0,0 +1,14 @@
+let num = 100
+
+if(num > 10) {        /* 'se' condição */
+    console.log("Numero é maior que 10")
+    if(num > 50) {
+        console.log("Numero é maior que 50")
+    }
+} else if(num > 5) {        /* 'se' outra condição */
+    console.log("Numero está ente 6 e 10")        
+} else {
+    console.log("Numero menor ou igual a 5")
+}
+
+console.log("Fim do programa")
