832167ec165e5cf699e01314903151772a512d5d
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-08 20:00:30 -0300

    criacao_de_comando_de_decisao_com_dois_testes_logicos

---
+++ if__else.js
@@ -0,0 +1,11 @@
+let num = 10
+
+if(num > 10) {        /* 'se' condição */
+    console.log("Numero é maior que 10")
+} else if(num > 5) {        /* 'se' outra condição */
+    console.log("Numero está ente 6 e 10")        
+} else {
+    console.log("Numero menor ou igual a 5")
+}
+
+console.log("Fim do programa")
