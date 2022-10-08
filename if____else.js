e62a56e2b286f2a46500601ea8bf8aa0455432a8
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-08 20:20:20 -0300

    ultilizando_oprerador_relacional_'end'_no_comando_de_decisao

---
+++ if____else.js
@@ -0,0 +1,10 @@
+let energia = 100
+let clima = "chuvendo"
+
+if(energia > 70 && clima == "sol") {
+    console.log("Praia")
+} else {
+    console.log("Cinema")
+}
+
+console.log("Fim do programa")
