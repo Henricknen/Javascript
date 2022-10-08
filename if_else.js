db8abae5e0589c772102556e18f12b11902ae4ad
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-08 19:30:43 -0300

    criando_comando_de_decisao_'if'_e_'else'

---
+++ if_else.js
@@ -0,0 +1,9 @@
+let num = 10
+
+if(num > 10) {        /* 'se' condição */
+    console.log("Numeral é maior que 10")
+} else {
+    console.log("Numeral é menor ou igual a 10")        /* 'senão' condição */
+}
+
+console.log("Fim do programa")
