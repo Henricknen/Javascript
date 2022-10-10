cc14ea31132b4827c8723bc53b491fb97b3fecad
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-10 13:57:04 -0300

    fazendo_logica_de_variavel_par_ou impar_ultilizando_'for'

---
+++ loop__for.js
@@ -0,0 +1,9 @@
+console.log("Inicio do programa")
+
+for(let i = 0; i < 100; i++) {
+    if(i % 2 == 0) {
+        console.log("O valor " + i + " é par")
+    } else {
+        console.log("O valor " + i + " é impar")
+    }
+}
