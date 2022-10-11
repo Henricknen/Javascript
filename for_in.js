94fd9212c8e392e99e31f77019062ae64db26733
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-10 22:46:34 -0300

    comparando_laco_de_repeticao_'for'_com_o_'for_in'

---
+++ for_in.js
@@ -0,0 +1,11 @@
+let num = [10, 20, 30, 40, 50]      /* array numerico */
+
+/* for in */
+for(n in num) {     /* 'n' é uma variavel que recebe os elementos do array 'num' */
+    console.log(num[n])     
+}
+
+/* for tradiçional */
+for(let i = 0; i < num.length; i++) {
+    console.log(num[i])     
+}
