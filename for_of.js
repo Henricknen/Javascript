8e6a79cd0402c889098135d82e5fd03c55114996
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-11 00:33:39 -0300

    ultilizando_'for_of'

---
+++ for_of.js
@@ -0,0 +1,11 @@
+let num = [10, 20, 30, 40, 50]      /* array numerico */
+
+/* for of */
+for(n of num) {     /* 'for of' pega os elementos da array 'num' */
+    console.log(n)     
+}
+
+/* for in */
+for(n in num) {     /* 'for in' pega as posições da array 'num' */
+    console.log(num[n])     
+}
