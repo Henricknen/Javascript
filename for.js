8a9a18719823473b47ba4ab61609bc6e16865709
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-10 22:33:17 -0300

    criando_array_'num'_e_fazendo_a_impressao_de_cada_elemento_dos_indices_de_'num'

---
+++ for.js
@@ -0,0 +1,5 @@
+let num = [10, 20, 30, 40, 50]      /* array numerico */
+
+for(let i = 0; i < num.length; i++) {
+    console.log(num[i])     /* imprimindo do array 'num' os elementos da posição 'i' */
+}
