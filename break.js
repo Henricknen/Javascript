250f84923654abb6364cdd7718ace6ef3d4c786a
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-12 20:59:48 -0300

    expressao_'break'_gera_uma_interrupcao_na_execucao_do_programa

---
+++ break.js
@@ -0,0 +1,11 @@
+let n = 0
+let max = 1000
+
+while(n < max) {
+    console.log("JavaScript " + n)
+    if(n > 10) {        /* se esta condição for 'true' o loop para */
+        break
+    }
+    n++
+}
+console.log("Fim...")
