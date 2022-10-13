a2a5dce3e0d1de5d5ef06fb7a60c2861147ea478
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-13 19:36:10 -0300

    criando_'funcao'_em_js

---
+++ funcoes.js
@@ -0,0 +1,7 @@
+function codigo() {       /* função tradiçional 'parametrizada' */
+    console.log("trabalhando com funções")
+}
+
+for(let i = 0; i < 10; i++) {       /* colocando a chamada dentro de um 'loop' */
+    codigo()        /* fazendo a chamada da função */
+}
