dce649fa4c95e11de8df7697583d78b99842962d
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-14 18:46:55 -0300

    criacao_de_funcao_que_'retorna'_o_resultado_de_processamento_feito_na_mesma

---
+++ retorno.js
@@ -0,0 +1,10 @@
+function Nome() {
+    let n1 = 10
+    let n2 = 8
+    let res = n1 * n2
+    return res      /* 'return' retorna o resultdado de 'res' quando chamar a função */
+}
+
+console.log(Nome())     /* 'console.log' vai imprimir o 'retorno' da função */
+console.log(Nome()) 
+console.log(Nome()) 
