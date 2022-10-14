668e3c27f836bf8153aa0472f12d1a37583595e1
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-13 22:15:14 -0300

    ultilizando_funcao_para_alterar_texto_das_'div's

---
+++ funcao/funcoes..js
@@ -0,0 +1,13 @@
+function mudarTexto() {     /*  */
+    let d1 = document.getElementById("d1")
+    let d2 = document.getElementById("d2")
+    let d3 = document.getElementById("d3")
+    let d4 = document.getElementById("d4")
+
+    d1.innerHTML = "Luis Henrique Silva Ferreira"
+    d2.innerHTML = "Luis Henrique Silva Ferreira"
+    d3.innerHTML = "Luis Henrique Silva Ferreira"
+    d4.innerHTML = "Luis Henrique Silva Ferreira"
+    
+}
+
