ea6ee6d9a2cf574bc2cacb433f7da14b4bf600ae
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-14 19:19:34 -0300

    crinando_uma_variavel_''num'_que_recebera_o_retorno_da_funcao_'Multiplicacao'

---
+++ retorno..js
@@ -0,0 +1,9 @@
+function Multiplicacao() {
+    let n1 = 10
+    let n2 = 8
+    let res = n1 * n2
+    return res      /* 'return' retorna o resultdado de 'res' quando chamar a função */
+}
+
+let num = Multiplicacao()        /* variavel 'num' recebe o retorno da função 'Multiplicacao' */
+console.log(num)     /* 'console.log' vai imprimir a variável 'num' */
