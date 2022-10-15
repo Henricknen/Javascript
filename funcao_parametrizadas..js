2b7c844ba56a0ad319d3d5a38a1d774c13aa8e58
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-15 19:08:26 -0300

    executando_'soma'_com_dois_parametros_que_irar_receber_os_numeros_da_chamada_'soma()'

---
+++ funcao_parametrizadas..js
@@ -0,0 +1,5 @@
+function soma(n1, n2) {
+    console.log(n1 + n2)        /* o 'console.log' além de fazer a impressão fará o calculo da soma  */
+}
+
+soma(10, 8)     /* o valor '10' ira entrar no primeiro parametro e o valor '8' no segundo */
