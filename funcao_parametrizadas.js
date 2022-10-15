fcfee3fa9b2b215073de3a2cc6673c3ff2ace162
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-15 19:00:06 -0300

    funcao_'parametrizadas'sao_funcoes_onde_se_deve_usar_ou_entrar_com_valores_para_dentro_da_funcao

---
+++ funcao_parametrizadas.js
@@ -0,0 +1,7 @@
+function descricao(p1) {     /* o parametro 1 'p1' recebe o valor passado na 'chamada' da função  */
+    console.log(p1)         /* vai fazer a impressão do valor de 'p1' */
+}
+
+descricao("Luis henrique silva ferreira")    /* chamando a função soma */
+descricao("Repositorio de JavaScript")
+descricao("Ano " + 2022)          /* passando outro valor para a função */
