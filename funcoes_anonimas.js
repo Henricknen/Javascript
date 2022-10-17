cb3cce48038e18e6ae1dba23874c50fd146ea9ba
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-17 13:57:22 -0300

    criando_'funcoes_anonumas'_que_nao_possuem_um_nome_associado_ao_seu_'corpo'_conteudo

---
+++ funcoes_anonimas.js
@@ -0,0 +1,5 @@
+const f = function(v1, v2) {        /* por ser uma função anônima o nome dela será omitido e é precisso estar associado a uma variável */
+    return v1 + v2
+}
+
+console.log(f(10, 7))       /* chamada da função */
