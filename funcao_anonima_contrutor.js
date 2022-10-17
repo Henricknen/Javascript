12f4b80ef50ba3b1cc15103fde67ef9e9aace5c3
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-17 14:31:34 -0300

    criando_'funcao_anonima_construtor'

---
+++ funcao_anonima_contrutor.js
@@ -0,0 +1,3 @@
+const f = new Function("v1", "v2", "return v1 +  v2")    /* em 'função construtor anonima' ultilizar o operador 'new' */
+                 /* "argumentos" , "corpo da fução"*/ 
+console.log(f(12, 67))
