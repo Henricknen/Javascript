d16af111deee12882f407188bd50b156d3064b53
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-18 16:16:13 -0300

    se_for_apenas_um_paramentro_de_entrada_o_parenetezes_'()'_nao_e_necessario_se_for_uma_operacao_simples_nao_precisa_do_'return'_que_sem_este_na_funcao_as_chaves_'{}'_nao_e_necessaria

---
+++ funcao_lambida...js
@@ -0,0 +1,11 @@
+const soma =(v1, v2)=> v1 + v2  /* sem chaves '{}' */
+console.log(soma(34, 57))   /* quando não é ultizado 'return' não é obrigatorio ultilzaz as chaves '{}' */
+
+
+const nome =n=> n     
+console.log(nome("Luis Henrique Silva Ferrira"))       
+
+const add  =n=> n + 10          
+console.log(add(12))        
+
+
