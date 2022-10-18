2f8883a102312b4f11180f6bfb03212703ee02e0
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-18 15:54:42 -0300

    'arrow_function'_que_mostra_que_nao_e_preciso_colocar_parenetezes'= =>'_quando_se_tem_apenas_uma_passagem_de_valor

---
+++ funcao_lambida..js
@@ -0,0 +1,7 @@
+const soma =(v1, v2)=> {return v1 + v2}
+
+const nome =n=> {return n}      /* se entra com apenas um parâmetro não é necessario colocar o parentezes */
+
+console.log(soma(34, 57))
+
+console.log(nome("Luis Henrique Silva Ferrira"))        /* criando a chamada da função */
