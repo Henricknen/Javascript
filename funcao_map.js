f6c7185ba42e7edfbdda37048f6300e0d2e24b70
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-21 15:11:33 -0300

    funcao_'map'_tem_finalidade_de_percorre_array_e_trabalhar_com_os_elementos_ou_retorna-los_sem_interrupcao

---
+++ funcao_map.js
@@ -0,0 +1,4 @@
+const repositorios = ['HTML', 'SQL', 'JAVASCRIT', 'PHP', 'REACT']
+repositorios.map((el, i) => {      /* em 'map' é possivel passar dois parâmetros, 'el' é o elemento da coleção, 'i' é o indice deste elemento */
+    console.log("repositorios: " + el + " posição do repositorio " + i)
+})
