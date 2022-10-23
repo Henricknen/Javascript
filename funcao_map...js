f6900812c010cb7b14733b9588ee437fe82c658f
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-10-22 22:44:00 -0300

    ultilizando_fucao_'map'_com_'return'

---
+++ funcao_map...js
@@ -0,0 +1,6 @@
+const repositorios = ['HTML', 'SQL', 'JAVASCRIT', 'PHP', 'REACT']
+let r = repositorios.map((el, i) => {      /* Variável 'let r' que recebe o retorno do método 'map' */
+    return el   /* retornando o elemento */
+})
+
+console.log(r)      /* retorna o que será retornado por 'repositorio' */
