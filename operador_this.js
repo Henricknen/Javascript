a8b0ad5b94fc24f6ff47d8e79e319e8dd673e0e3
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-12-04 13:48:53 -0300

    criando_metodo_'settimeout'_dentro_da_funcao_e_uma_funcao_'arrow'_para_rodar_o_'settimeout'

--- operador_this.js
+++ operador_this.js
@@ -2,8 +2,21 @@ function aluno(nome, nota) {
     this.nome = nome        // 'this.' faz a distinção da variavel 'nome' com o parametro de entrada 'nome'
     this.nota = nota
 
-    console.log(nome)
-    console.log(nota)
+    this.dados_anonimo = function() {       // função anônima
+        setTimeout(function() {     // 'setTimeout' gera um atraso na execução
+            console.log(this.nome)
+            console.log(this.nota)
+        }, 2000)        // '2000' é o tempo do 'setTimeout'
+    }
+
+    this.dados_arrow = function() {       // função anônima
+        setTimeout(()=> {     // 'setTimeout' gera um atraso na execução
+            console.log(this.nome)
+            console.log(this.nota)
+        }, 2000)        // '2000' é o tempo do 'setTimeout'
+    }
 }
 
-aluno("Luis henrique", 10)      // chamando a função 'aluno' e passando o valor da nota
+const al1 = new aluno("Luis henrique", 10)      // passando o valor da nota
+al1.dados_anonimo()     // chamando a função 'tradiçional'
+al1.dados_arrow()     // chamando a função 'arrow'
