f321d06491a12145ade76e624ec2a54d83d12f60
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-12-10 19:46:22 -0300

    transferindo_elementos_nao_selecionados_para_container_de_origem

--- addEventListener/script.js
+++ addEventListener/script.js
@@ -1,6 +1,6 @@
 const caixa1 = document.querySelector("#caixa1")     // obtendo os elemento de id 'caixa1'
 const caixa2 = document.querySelector("#caixa2")
-const btn = document.querySelector("#btn_copiar")
+const btn_transferir = document.querySelector("#btn_transferir")
 const todosrepositorios = [...document.querySelectorAll(".repositorio")]      // pegando todos os reepositorios que usam a classe 'repositorio' e dando um 'spread' e transformando em um array
 
 todosrepositorios.map((el)=> {// 'map' adiçiona o evento de 'click' em todos 'repositorios'
@@ -10,9 +10,14 @@ todosrepositorios.map((el)=> {// 'map' adiçiona o evento de 'click' em todos 'r
     })
 })
 
-btn.addEventListener("click", ()=> {        // quando botão for clicado os elementos que tem classe 'selecionado' seá adiçionado em 'caixa2'
-    const repositoriosselecionados = [...document.querySelectorAll(".selecionado")]
-    repositoriosselecionados.map((el)=> {        // percorrendo o 'araay' todo com 'map'
+btn_transferir.addEventListener("click", ()=> {        // quando botão for clicado os elementos que tem classe 'selecionado' seá adiçionado em 'caixa2'
+    const repositoriosSelecionados = [...document.querySelectorAll(".selecionado")]
+    const repositoriosNaoSelecionados = [...document.querySelectorAll(".repositorio:not(.selecionado)")]        // lista elementos da classe 'repositorio' que não 'not()' tem a classe 'selecionado'
+    repositoriosSelecionados.map((el)=> {        // percorrendo o 'array' todo com 'map'
         caixa2.appendChild(el)        // 'appendChild' anexa o filho 'el' ao 'caixa2'
+    })
+
+    repositoriosNaoSelecionados.map((el)=> {        
+        caixa1.appendChild(el)      // 'apendChild' retorna com elementos de 'repositoriosNaoSelecionados(el)' para 'caixa1'
     })
 })
