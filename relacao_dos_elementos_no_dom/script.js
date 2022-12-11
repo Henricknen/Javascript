854503374aad9b99a7814cc8dbf45eb1d51e1c6a
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-12-11 17:07:39 -0300

    referenciando_elementos_do_'DOM'

---
+++ relacao_dos_elementos_no_dom/script.js
@@ -0,0 +1,8 @@
+const caixa1 = document.querySelector("#caixa1")
+const btn_c = [...document.querySelectorAll(".repositorio")]
+
+console.log(document.getRootNode())      // pegando o nó 'raiz' ultilizando método 'getRootNode''
+
+console.log(caixa1.firstElementChild)      // pegando o primeiro elemento de 'caixa1'
+console.log(caixa1.lastElementChild)            // pegando o ultimo elemento de 'caixa1'
+console.log(caixa1.children)        // pegando todos elementos ultilizando 'children'
