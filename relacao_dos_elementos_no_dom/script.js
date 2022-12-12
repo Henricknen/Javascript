4ffe03934e829bd57e9f4dfc3f98863b5285bba2
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-12-12 00:20:05 -0300

    ultilizando_elementos_filhos'child'_e_'children'_elementos_raiz'root'_e_elementos_pais_'parentNode'

--- relacao_dos_elementos_no_dom/script.js
+++ relacao_dos_elementos_no_dom/script.js
@@ -1,8 +1,23 @@
 const caixa1 = document.querySelector("#caixa1")
 const btn_c = [...document.querySelectorAll(".repositorio")]
 
-console.log(document.getRootNode())      // pegando o nó 'raiz' ultilizando método 'getRootNode''
+const c1_2 = document.querySelector('#c1_2')        // pegando 'div' de 'id' 'c1_2'
+// console.log(c1_2)
+console.log(c1_2.parentNode.parentNode)         // 'parentNode' re torna o pai 'elemento anterior'
 
-console.log(caixa1.firstElementChild)      // pegando o primeiro elemento de 'caixa1'
-console.log(caixa1.lastElementChild)            // pegando o ultimo elemento de 'caixa1'
-console.log(caixa1.children)        // pegando todos elementos ultilizando 'children'
+// console.log(caixa1.hasChildNodes())       // método 'hasChildNodes' retorna 'true' se o elemento possui um filho
+// console.log(btn_c[0].hasChildNodes())           // retornando o pimeiro '[0]' elemento dos btões
+// console.log(btn_c[0].childNodes)       // 'childNodes' retorna um 'text'
+// console.log(btn_c[0].children)      // 'children' retorna os elementos da coleção
+
+// console.log(btn_c[0].children.length > 0 ? "Possui filhos" : "Não possui filhos")      // condição ternaria
+
+// console.log(caixa1.firstElementChild.innerHTML = "alterando primeiro elemnto")       // operando elemento 'firstElementChild' do 'caixa1'
+// console.log(caixa1.children[1].innerHTML = "alterando 'filho' da lista")     // 'children' retorna uma lista com os filhos
+
+
+// console.log(document.getRootNode())      // pegando o nó 'raiz' ultilizando método 'getRootNode''
+
+// console.log(caixa1.firstElementChild)      // pegando o primeiro elemento de 'caixa1'
+// console.log(caixa1.lastElementChild)            // pegando o ultimo elemento de 'caixa1'
+// console.log(caixa1.children)        // pegando todos elementos ultilizando 'children'
