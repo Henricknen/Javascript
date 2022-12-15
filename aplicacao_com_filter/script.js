664759b34e83b478ba2b1ec1cbb01e386ba74ba0
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-12-15 01:10:01 -0300

    criando_funcao_de_'remocao'_e_um_'alert'_caso_o_elemento_nao_esteje_selecionado

--- aplicacao_com_filter/script.js
+++ aplicacao_com_filter/script.js
@@ -1,8 +1,9 @@
-const caixaCursos=document.querySelector("#caixaCursos")
-const btn_c=[...document.querySelectorAll(".curso")]
-const c1_2=document.querySelector("#c1_2")
-const cursos=["HTML","CSS","Javascript","PHP","React","MySQL","ReactNative"]
-const btnCursoSelecionado=document.getElementById("btnCursoSelecionado")
+const caixaCursos = document.querySelector("#caixaCursos")
+const btn_c = [...document.querySelectorAll(".curso")]
+const c1_2 = document.querySelector("#c1_2")
+const cursos = ["HTML","CSS","Javascript","PHP","React","MySQL","ReactNative"]
+const btnCursoSelecionado = document.getElementById("btnCursoSelecionado")      // botão de 'seleção' de curso
+const btnRemoveCurso = document.getElementById("btnRemoveCurso")        // botão de 'remoção' de curso
 
 cursos.map((el,chave)=>{
     const novoElemento=document.createElement("div")
@@ -25,18 +26,34 @@ cursos.map((el,chave)=>{
     
 })
 
-btnCursoSelecionado.addEventListener("click",(evt)=>{       // ultilizando evento de 'click'
+const radioSelecionado=()=> {
     const todosRadios=[...document.querySelectorAll("input[type=radio]")]       // ultilizando 'spread' transformando em 'array' e pegando so os input do tipo 'radio'
-    let radioSelecionado=todosRadios.filter((ele,ind,arr)=>{        // fazendo filtragem peguando todos 'radios' seleçionados
+    let radioSelecionado=todosRadios.filter((ele,ind,arr)=>{        // fazendo filtragem pegando todos 'radios' seleçionados
         return ele.checked      
     })
-    radioSelecionado=radioSelecionado[0]        
-    // const cursoSelecionado=radioSelecionado.parentNode.parentNode.firstChild.textContent
-    const cursoSelecionado=radioSelecionado.parentNode.previousSibling.textContent
-    alert("Curso selecionado: " + cursoSelecionado)
-    // console.log(todosRadios)
-    // console.log(radioSelecionado)
-    // console.log(cursoSelecionado)
+    return radioSelecionado[0]    
+}
+
+btnCursoSelecionado.addEventListener("click",(evt)=>{       // ultilizando evento de 'click'
+    const rs = radioSelecionado()          // 'radioSlecionado' vai obter o conteudo da função 'cursoSelecionado'
+    if(rs != undefined) {
+        const cursoSelecionado = rs.parentNode.previousSibling.textContent
+        alert("Curso selecionado: " + cursoSelecionado)
+    } else {
+        alert("Seleçione um repositorio...")
+    }
+    
+})
+    
+btnRemoverCurso.addEventListener("click",(evt)=>{       // ultilizando evento de 'click'
+    const rs = radioSelecionado()          // 'radioSlecionado' vai obter o conteudo da função 'cursoSelecionado'
+    if(rs != undefined) {
+        const cursoSelecionado = rs.parentNode.parentNode   // 'parentNode' vai devolver o que será removido
+        cursoSelecionado.remove()      // chamando a função de remoção para remoção do elemento seleçionado
+    } else {
+        alert("Seleçione um repositorio...")
+    }
+    
 })
 
 // parentNode
@@ -45,4 +62,3 @@ btnCursoSelecionado.addEventListener("click",(evt)=>{       // ultilizando event
 // lastChild
 // nextSibling
 // previousSibling
-
