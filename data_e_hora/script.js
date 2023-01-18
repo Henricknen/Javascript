5f06360714f9f98e0dca5e183d6c4bf3bf5ce1b3
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-18 00:19:48 -0300

    criando_um_Timer_com_Alarme_em_Javascript_usando_os_metodos_e_propriedades_de_Data_e_Hora_da_'classe_Date'

--- data_e_hora/script.js
+++ data_e_hora/script.js
@@ -1,5 +1,37 @@
 const div_data = document.getElementById("div_data")
 const div_relogio = document.getElementById("div_relogio")
+const btn_ativar = document.getElementById("btn_ativar")
+const btn_parar = document.getElementById("btn_parar")
+const tmp_alarme = document.getElementById("tmp_alarme")
+const hora_alarme = document.getElementById("hora_alarme")
+const timer = document.getElementById("timer")
+
+const som_alarme = new Audio("alarme.mp3")        // apontando para o arquivo de 'audio'
+som_alarme.loop = -1        // repetição do toque de alarme infinitamente '-1'
+
+let td_atual = null     // variáveis
+let ts_alarme = null
+let alarme_ativado = false
+let alarme_tocando = false
+
+btn_ativar.addEventListener("click", ()=> {
+    td_atual = Date.now()
+    ts_alarme = td_atual + (tmp_alarme.value * 1000)
+    alarme_ativado = true
+    const dt_alarme = new Date(ts_alarme)  
+    hora_alarme.innerHTML = "Hora do Alarme:" + dt_alarme.getHours() + ":" + dt_alarme.getMinutes() + ":" + dt_alarme.getSeconds()
+})
+
+btn_parar.addEventListener("click", ()=> {
+    alarme_ativado = false
+    alarme_tocando = false
+    hora_alarme.innerHTML = "Hora do Alarme:"
+    tmp_alarme.value = 0
+    timer.classList.remove("alarme")
+    som_alarme.pause();
+    som_alarme.currentTime = 0;
+
+})
 
 const data = new Date()     // constante 'data' instançiando um objeto data da classe Date
 
@@ -26,6 +58,15 @@ const relogio=()=> {
 
     const hora_completa = hora +":" +minutos+ ":" +segundos
     div_relogio.innerHTML = hora_completa       // atualizando o valor da 'div_relogio'
+
+    if(alarme_ativado && !alarme_tocando) {
+        if(data.getTime() >= ts_alarme) {
+            alarme_tocando = true
+            som_alarme.play()
+            timer.classList.add("alarme")
+        }
+    }
+
     
 }
 
