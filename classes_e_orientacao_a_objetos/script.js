c4c9aa99722707ac73323d4b21b665f6bd095666
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-23 20:02:26 -0300

    criando_velocidade_'x'_'y'_direcao_da_bolinha_funcao_e_metodos

--- classes_e_orientacao_a_objetos/script.js
+++ classes_e_orientacao_a_objetos/script.js
@@ -11,13 +11,38 @@ let bolas = []      // array onde será guardado os objetos instançiados
 let numBola = 0
 
 class Bola {        // classe 'Bola'
-    constructor() {
+    constructor(arrayBolas, palco) {
         this.tam = Math.floor(Math.random() * 15) + 10  // definido o tamanho da bolinha
         this.r = Math.floor(Math.random() * 255)
         this.g = Math.floor(Math.random() * 255)        // sorteando propriedade de cores
         this.p = Math.floor(Math.random() * 255)
         this.px = Math.floor(Math.random() * (larguraPalco - this.tam))     // local onde as 'bolinhas' irão nascer
         this.py = Math.floor(Math.random() * (alturaPalco - this.tam))          // local onde as 'bolinhas' irão nascer
+        this.velx = Math.floor(Math.random() * 2) + 0.5     // criando 'veloçidade' x
+        this.vely = Math.floor(Math.random() * 2) + 0.5         // criando 'veloçidade' y
+        this.dirx = Math.random() * 10 > 5 ? 1:-1
+        this.diry = Math.random() * 10 > 5 ? 1:-1       // direção
+        this.palco = palco
+        this.arrayBolas = arrayBolas
+        this.id = Date.now() + "_" + Math.floor(Math.random() * 100000000)
+        this.desenhar()     // chamando a função 'desenhar'
+        this.controle = setInterval(this.controlar, 10)     // 'setInterval' chama a função controlar a cada 10 segundos
+        this.eu = document.getElementById(this.id)
+    }
+
+    minhaPos=()=> {     // métodos
+        
+    }
+    remover=()=> {
+
+    }
+
+    desenhar=()=> {
+
+    }
+
+    controlar=()=> {
+
     }
 }
 
