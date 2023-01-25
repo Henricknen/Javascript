4d90040d807056eb07648fae1820d0d7b144ad0c
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-24 22:07:11 -0300

    finalizacao_do_projeto_de_'classes_e_orientacao_de_objetos'_em_javascript

--- classes_e_orientacao_a_objetos/script.js
+++ classes_e_orientacao_a_objetos/script.js
@@ -15,7 +15,7 @@ class Bola {        // classe 'Bola'
         this.tam = Math.floor(Math.random() * 15) + 10  // definido o tamanho da bolinha
         this.r = Math.floor(Math.random() * 255)
         this.g = Math.floor(Math.random() * 255)        // sorteando propriedade de cores
-        this.p = Math.floor(Math.random() * 255)
+        this.b = Math.floor(Math.random() * 255)
         this.px = Math.floor(Math.random() * (larguraPalco - this.tam))     // local onde as 'bolinhas' irão nascer
         this.py = Math.floor(Math.random() * (alturaPalco - this.tam))          // local onde as 'bolinhas' irão nascer
         this.velx = Math.floor(Math.random() * 2) + 0.5     // criando 'veloçidade' x
@@ -38,12 +38,12 @@ class Bola {        // classe 'Bola'
     remover=()=> {
         clearInterval(this.controle)        // para a bolinha na tela
         bolas = bolas.filter((b)=> {
-            if(b. id != this.id) {
+            if(b.id != this.id) {
                 return b
             }
         })
         this.eu.remove()        // removendoo do 'DOM'
-        numBola--       // atualizando o array com o número de bolas
+        // numBola--       // atualizando o array com o número de bolas
         num_objetos.innerHTML = numBola     // atualizando o 'display'
 
     }
@@ -52,12 +52,32 @@ class Bola {        // classe 'Bola'
         const div = document.createElement("div")       // crianção da div 'bolinha'
         div.setAttribute("id", this.id)
         div.setAttribute("class", "bola")
-        div.setAttribute("style", `left: ${this.px}; top: ${this.py}; width: ${this.tam}; height: ${this.tam}; background-color: rgb(${this.r}, ${this.g}, ${this.b}`)      // adiçionado 'style' de todas propriedades definas como variáveis
+        div.setAttribute("style", `left: ${this.px}px; top: ${this.py}px; width: ${this.tam}px; height: ${this.tam}px; background-color: rgb(${this.r}, ${this.g}, ${this.b}`)      // adiçionado 'style' de todas propriedades definas como variáveis
         this.palco.appendChild(div)        // adiçionando no 'DOM'
     }
 
-    controlar=()=> {
+    controle_bordas=()=> {
+        if(this.px + this.tam >= larguraPalco) {
+            this.dirx = -1      // se a bolinha chegou no limite direito ela será mandada para esquerda
+        } else if(this.px <= 0) {
+            this.dirx = 1       // se a bolinha chegou no limite esquerdo ela será mandada para direita
+        }
 
+        if(this.py + this.tam >= larguraPalco) {
+            this.diry = -1
+        } else if(this.py <= 0) {
+            this.diry = 1
+        }
+    }
+
+    controlar=()=> {
+        this.controle_bordas()      // verifiacando se as bolinhas encostou nas 'bordas'
+        this.px += this.dirx * this.velx    // atualizando nova posição 'x'
+        this.py += this.diry * this.vely        // atualizando nova posição 'y'
+        this.eu.setAttribute("style", `left: ${this.px}px; top: ${this.py}px; width: ${this.tam}px; height: ${this.tam}px; background-color: rgb(${this.r}, ${this.g}, ${this.b}`)      // atualizando a 'bolinha' do DOM
+        if((this.px > larguraPalco) || (this.py > alturaPalco)) {
+            this.remover()      // chamada da função 'remover'
+        }
     }
 }
 
@@ -66,15 +86,15 @@ window.addEventListener("resize", (evt)=> {     // evento de redimensionamento d
     let alturaPalco = palco.offsetHeight            // variável 'alturaPalco'
 })
 
-btn_add.aaEventListener("click", (evt)=> {      // evento de adição
+btn_add.addEventListener("click", (evt)=> {      // evento de adição
     const qtde = txt_qtde.value     // obtendo o valor das bolinhas de 'txt_qtde'
     for(let i = 0; i < qtde; i++) {
-        // instançiar novas bolinhas
+        bolas.push(new Bola(bolas, palco))      // instançiar novas bolinhas
     }
 })
 
-// // btn_remover.aaEventListener("click", (evt)=> {      // remoção de adição
-// //     bolas.map()=> {
-// //         // remover a bolinhao
-// //     }
-// })
+btn_remover.addEventListener("click", (evt)=> {      // remoção de adição
+    bolas.map((b)=> {
+        b.remover()     // remover a bolinhao
+    })
+})
